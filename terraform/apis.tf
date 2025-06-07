c954cba191bd8937d492f8ef35266b949e75170e
dobb1n <dobbinwondermule@gmail.com>
2025-06-07 14:59:08 +0100

    iam api into the tf and changing entry point for the cf

--- terraform/apis.tf
+++ terraform/apis.tf
@@ -4,3 +4,10 @@ resource "google_project_service" "cloud_scheduler" {
 
   disable_on_destroy = false
 }
+
+resource "google_project_service" "iam" {
+  service = "iam.googleapis.com"
+  project = var.project_id
+
+  disable_on_destroy = false
+}
